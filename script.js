d07d9b4e910bab838d74bace95a8b8a13fbba497
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-02-14 18:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+const output = document.getElementById('output');
+
+function updateOutput() {
+  output.textContent = window.location.href;
+  event.preventDefault();
+}
+
+const submitButton = document.getElementById('button');
+submitButton.addEventListener('click', updateOutput);
